a1ae715cf612a78c0765453965b6d29ff927cca3
mchairul <email.muhammad.chairul@gmail.com>
2020-02-18 14:02:27 +0700

    till request to backend
    till request to backend

---
+++ app/src/main/java/com/example/citcalljavaexample/URLs.java
@@ -0,0 +1,9 @@
+package com.example.citcalljavaexample;
+
+public class URLs {
+
+    private static final String API_URL = "http://192.168.1.67/backend/";
+
+    public static final String URL_REQUEST = API_URL + "mrequest.php";
+
+}
